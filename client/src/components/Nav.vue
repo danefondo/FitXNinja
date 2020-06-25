4e2e6f9f3e65bf2e7c0131972f8930e0cd7e4f5e
Robert Unyx <robertunyx@gmail.com>
2020-06-25 21:27:13 +0300

    nav

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,127 +1,74 @@
-<template>
-  <div class="nav-container" :class="isAuthenticated ? 'authNav' : 'notAuthNav'">
-    <div class="title-container">
-      <router-link to="/">
-      <img class="fitxlogosmall" :src="ninjaImage" />
-      </router-link>
-      <!-- <router-link class="title-btn" to="/">FitX Ninja</router-link> -->
-    </div>
-    <div class="navlinks-container">
-      <router-link
-        class="topNavigationLink__topBar desktopSize"
-        to="/scheduled"
-      >Discover</router-link>
-      <router-link class="topNavigationLink__topBar" to="/discover">Browse</router-link>
-      <template v-if="isAuthenticated">
-        <div class="stream_buttons desktopSize">
-          <router-link
-            v-if="user.is_live && user.active_stream_id"
-            class="go_live_button isOnAir"
-            :to="'/watch/' + user.active_stream_id"
-          >{{ $t("nav.is-live") }}</router-link>
-          <router-link
-            v-if="!user.is_live"
-            class="go_live_button goOnAir"
-            to="/golive"
-          >Start a workout</router-link>
-        </div>
-        <div class="logout-container" :class="isAuthenticated ? 'authProfile' : 'notAuthProfile'">
-          <div
-            ref="dropdown"
-            @click="toggleDropdown()"
-            class="DropdownX"
-            :class="{'is-expanded': isOpened}"
-          >
-            <div class="Dropdown-profile">
-              <div class="Photo">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-                  <path
-                    style="fill:#00ffe7; text-indent:0;text-align:start;line-height:normal;text-transform:none;block-progression:tb;-inkscape-font-specification:Bitstream Vera Sans"
-                    d="M 16 5 C 12.145852 5 9 8.1458513 9 12 C 9 14.408843 10.23116 16.55212 12.09375 17.8125 C 8.5266131 19.342333 6 22.881262 6 27 L 8 27 C 8 22.569334 11.569334 19 16 19 C 20.430666 19 24 22.569334 24 27 L 26 27 C 26 22.881262 23.473387 19.342333 19.90625 17.8125 C 21.76884 16.55212 23 14.408843 23 12 C 23 8.1458513 19.854148 5 16 5 z M 16 7 C 18.773268 7 21 9.2267317 21 12 C 21 14.773268 18.773268 17 16 17 C 13.226732 17 11 14.773268 11 12 C 11 9.2267317 13.226732 7 16 7 z"
-                    color="#000"
-                    overflow="visible"
-                    font-family="Bitstream Vera Sans"
-                  />
-                </svg>
-              </div>
-              <div class="userId" id="5e770037c84aaa1088d5315c"></div>
-            </div>
-            <nav class="Dropdown-nav SettingsNav">
-              <ul class="Dropdown-group">
-                <li class="dropLive">
-                  <router-link
-                    class="entypo-newspaper NavLinkX"
-                    to="/golive"
-                  >Start a workout</router-link>
-                  <a class="entypo-plus OptionLink" href="#"></a>
-                </li>
-                <li>
-                  <router-link
-                    class="entypo-newspaper NavLinkX"
-                    :to="myProfile"
-                  >{{$t("nav.profile")}}</router-link>
-                  <a class="entypo-plus OptionLink" href="#"></a>
-                </li>
-                <li v-if="user.superadmin">
-                  <router-link
-                    class="entypo-archive NavLinkX"
-                    to="/master"
-                  >{{$t("admin.superadmin")}}</router-link>
-                  <a class="entypo-plus OptionLink" href="#"></a>
-                </li>
-                <li>
-                  <router-link
-                    class="entypo-archive NavLinkX"
-                    to="/manage-streams"
-                  >{{$t("nav.manage-streams")}}</router-link>
-                  <a class="entypo-plus OptionLink" href="#"></a>
-                </li>
-                <li>
-                  <router-link class="entypo-archive NavLinkX" to="/settings">{{$t("nav.settings")}}</router-link>
-                  <a class="entypo-plus OptionLink" href="#"></a>
-                </li>
-                <li>
-                  <a class="entypo-logout NavLinkX" @click="logout">{{$t("nav.logout")}}</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </template>
-      <template v-else>
-        <div class="stream_buttons">
-          <router-link
-            class="go_live_button login-nav login-responsive"
-            to="/login"
-          >{{$t("nav.login")}}</router-link>
-          <router-link class="go_live_button" to="/register">{{$t("nav.register")}}</router-link>
-        </div>
-      </template>
-    </div>
-    <div v-if="isAuthenticated" class="stream_buttons mobileSize">
-      <router-link
-        v-if="user.is_live && user.active_stream_id"
-        class="go_live_button isOnAir"
-        :to="'/watch/' + user.active_stream_id"
-      >{{ $t("nav.is-live") }}</router-link>
-      <router-link
-        v-if="!user.is_live"
-        class="go_live_button goOnAir"
-        to="/golive"
-      >{{ $t("nav.go-live") }}</router-link>
-    </div>
-  </div>
+<template lang="pug">
+	.nav-container(:class="isAuthenticated ? 'authNav' : 'notAuthNav'")
+		.deleteStreamModal(v-if='startWorkoutModal')
+			.deleteStreamModalBackground(@click='startWorkoutModal=false')
+			.deleteStreamModalWrapper
+				.deleteStreamContentWrapper
+					.deleteStreamTextContainer
+						.deleteStreamModalTitle {{ $t("newworkout.video-url") }}
+						.deleteStreamModalBody {{ $t("newworkout.start-custom") }}
+					input(v-model="custom_url" class="stream_input stream_name" :placeholder="$t('newworkout.link-placeholder')" max-length="50")
+					.action-group
+						.button-filled.neon-green(@click='startCustomWorkout') {{ $t("newworkout.start-workout") }}
+						.cancelDeleteStream.button-outline(@click='startWorkoutModal=false') {{ $t("editvideo.cancel-delete") }}
+		.title-container
+			router-link(to='/')
+				img.fitxlogosmall(:src='ninjaImage')
+			// <router-link class="title-btn" to="/">FitX Ninja</router-link>
+		.navlinks-container
+			router-link.topNavigationLink__topBar.desktopSize(to='/scheduled') Discover
+			router-link.topNavigationLink__topBar(to='/discover') Browse
+			template(v-if='isAuthenticated')
+				.stream_buttons.desktopSize
+					router-link.go_live_button.isOnAir(v-if='user.is_live && user.active_stream_id', :to="'/watch/' + user.active_stream_id") {{ $t("nav.is-live") }}
+					.go_live_button.goOnAir(@click='startWorkoutModal=true') Start a workout
+				.logout-container(:class="isAuthenticated ? 'authProfile' : 'notAuthProfile'")
+					.DropdownX(ref='dropdown', @click='toggleDropdown()', :class="{'is-expanded': isOpened}")
+						.Dropdown-profile
+							.Photo
+								svg(xmlns='http://www.w3.org/2000/svg', viewBox='0 0 30 30')
+									path(style='fill:#00ffe7; text-indent:0;text-align:start;line-height:normal;text-transform:none;block-progression:tb;-inkscape-font-specification:Bitstream Vera Sans', d='M 16 5 C 12.145852 5 9 8.1458513 9 12 C 9 14.408843 10.23116 16.55212 12.09375 17.8125 C 8.5266131 19.342333 6 22.881262 6 27 L 8 27 C 8 22.569334 11.569334 19 16 19 C 20.430666 19 24 22.569334 24 27 L 26 27 C 26 22.881262 23.473387 19.342333 19.90625 17.8125 C 21.76884 16.55212 23 14.408843 23 12 C 23 8.1458513 19.854148 5 16 5 z M 16 7 C 18.773268 7 21 9.2267317 21 12 C 21 14.773268 18.773268 17 16 17 C 13.226732 17 11 14.773268 11 12 C 11 9.2267317 13.226732 7 16 7 z', color='#000', overflow='visible', font-family='Bitstream Vera Sans')
+							#5e770037c84aaa1088d5315c.userId
+						nav.Dropdown-nav.SettingsNav
+							ul.Dropdown-group
+								li.dropLive
+									.go_live_button.goOnAir(@click='startWorkoutModal=true') Start a workout
+									a.entypo-plus.OptionLink(href='#')
+								li
+									router-link.entypo-newspaper.NavLinkX(:to='myProfile') {{$t("nav.profile")}}
+									a.entypo-plus.OptionLink(href='#')
+								li(v-if='user.superadmin')
+									router-link.entypo-archive.NavLinkX(to='/master') {{$t("admin.superadmin")}}
+									a.entypo-plus.OptionLink(href='#')
+								li
+									router-link.entypo-archive.NavLinkX(to='/manage-streams') {{$t("nav.manage-streams")}}
+									a.entypo-plus.OptionLink(href='#')
+								li
+									router-link.entypo-archive.NavLinkX(to='/settings') {{$t("nav.settings")}}
+									a.entypo-plus.OptionLink(href='#')
+								li
+									a.entypo-logout.NavLinkX(@click='logout') {{$t("nav.logout")}}
+			template(v-else='')
+				.stream_buttons
+					router-link.go_live_button.login-nav.login-responsive(to='/login') {{$t("nav.login")}}
+					router-link.go_live_button(to='/register') {{$t("nav.register")}}
+		.stream_buttons.mobileSize(v-if='isAuthenticated')
+			router-link.go_live_button.isOnAir(v-if='user.is_live && user.active_stream_id', :to="'/watch/' + user.active_stream_id") {{ $t("nav.is-live") }}
+			router-link.go_live_button.goOnAir(v-if='!user.is_live', to='/golive') {{ $t("nav.go-live") }}
 </template>
 
 <script>
 import auth from "../config/auth";
+import axios from "axios";
 import ninjaIcon from "../assets/images/whiteninjalogo.png";
 
 export default {
   name: "Nav",
   data() {
     return {
-      isOpened: false
+      isOpened: false,
+      startWorkoutModal: false,
+      custom_url: ""
     };
   },
   props: {
@@ -163,6 +110,21 @@ export default {
         this.toggleDropdown();
       }
     },
+    async startCustomWorkout() {
+      try {
+        let roomData = {};
+        roomData.host_id = this.user._id;
+        roomData.video_id = this.video._id;
+        roomData.video_url = this.video.url;
+        roomData.youtube_id = this.video.video_id;
+        roomData.date_created = new Date();
+        const result = await axios.post(`workoutRooms/createNewRoom`, roomData);
+        console.log("res", result);
+        this.$router.push(`/rooms/${result.data.room._id}`);
+      } catch (error) {
+        console.log("error", error);
+      }
+    },
     logout() {
       auth.logout();
     }
@@ -171,6 +133,20 @@ export default {
 </script>
 
 <style>
+
+.stream_input {
+	width: 275px;
+}
+
+.neon-green {
+	margin-right: 12px;
+	color: #000 !important;
+	background-color: #00ffe7 !important;
+}
+
+.neon-green:hover {
+	background-color: #00ffe5a9 !important;
+}
 
 .Dropdown-profile:hover {
   background-color: #272727;
